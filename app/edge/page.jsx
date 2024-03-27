42db5dbe53d98c4f3cd24896d2e385223a499c6d
Elad Rosenheim <elad.rosenheim@gmail.com>
2024-03-27 12:37:02 +0200

    Update page.jsx

--- app/edge/page.jsx
+++ app/edge/page.jsx
@@ -8,7 +8,7 @@ export default function FallbackPage() {
     return (
         <section className="mb-16 sm:mb-24">
             <header className="mb-16 sm:mb-24">
-                <h1 className="mb-6 text-4xl font-bold sm:text-5xl">You've reached the fallback page.</h1>
+                <h1 className="mb-6 text-4xl font-bold sm:text-5xl">You&apos;ve reached the fallback page.</h1>
                 <p>This page is using a Netlify Edge Function to rewrite the URL based on visitor geography.</p>
                 <p>For it to run, please either run locally with <code>netlify dev</code> or deploy to Netlify!</p>
                 <p>Alternatively, you can use Next.js Middleware, which is also automatically deployed on Netlify as an Edge Function.
