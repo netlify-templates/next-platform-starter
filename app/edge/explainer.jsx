c0f30a42ec6e2ba946a5d8a92e6d203da0a6f8ed
Elad Rosenheim <elad.rosenheim@gmail.com>
2024-03-31 18:43:43 +0300

    Edge function explainer: use Markdown, don't load file

--- app/edge/explainer.jsx
+++ app/edge/explainer.jsx
@@ -1,20 +1,24 @@
-import { promises as fs } from 'fs';
-import Link from 'next/link';
-import { CodeBlock } from '../../components/code-block';
+import { Markdown } from "components/markdown";
 
-const edgeFunctionFile = './netlify/edge-functions/rewrite.js';
+const explainer = `
+This page is using a Netlify Edge Function (\`netlify/edge-functions/rewrite.js\`) to rewrite the URL based on visitor geography.
 
-export default async function EdgeFunctionExplainer() {
-    const code = (await fs.readFile(edgeFunctionFile)).toString();
-    return (
-        <>
-            <p className="mb-6">This page is using a Netlify Edge Function to rewrite the URL based on visitor geography.</p>
-            <CodeBlock lineNumbers lang="js" title={edgeFunctionFile} code={code} />
-            <p className="mt-8">
-                <Link href="https://edge-functions-examples.netlify.app" className="transition link hover:opacity-80">
-                    See more examples
-                </Link>
-            </p>
-        </>
-    );
+~~~js
+const rewrite = async (request, context) => {
+    const path = context.geo?.country?.code === 'AU' ? '/edge/australia' : '/edge/not-australia';
+    return new URL(path, request.url);
+};
+
+export const config = {
+    path: '/edge'
+};
+
+export default rewrite;
+~~~
+
+[See more examples](https://edge-functions-examples.netlify.app)
+`;
+
+export default function EdgeFunctionExplainer() {
+    return <Markdown content={explainer} />    
 }
