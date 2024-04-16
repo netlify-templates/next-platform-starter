323e72c64214fde8506f102be66dd0860a1762f4
Elad Rosenheim <elad.rosenheim@gmail.com>
2024-04-16 11:52:34 +0300

    Forms support

--- components/header.jsx
+++ components/header.jsx
@@ -12,7 +12,7 @@ const navItems = [
 
 export function Header() {
     return (
-        <nav className="flex flex-wrap items-center gap-4 pt-6 pb-16 sm:pt-12 sm:pb-24">
+        <nav className="flex flex-wrap items-center gap-4 pt-6 pb-12 sm:pt-12 md:pb-24">
             <Link href="/">
                 <Image src={netlifyLogo} alt="Netlify logo" />
             </Link>
