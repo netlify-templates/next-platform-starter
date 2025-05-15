81e3d891a83ee4bdd0502845f13f08b4ec7b3eda
rupalinaha123555 <rupali.naha@webskitters.com>
2025-05-15 17:05:07 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -28,7 +28,7 @@ export default function Page() {
         <div className="flex flex-col gap-12 sm:gap-16">
             <section>
                 <ContextAlert className="mb-6" />
-                <h1 className="mb-4">Netlify Platform Starter - Next.js</h1>
+                <h1 className="mb-4">Netlify Platform Starter - Next.js2</h1>
                 <p className="mb-6 text-lg">Get started with Next.js and Netlify in seconds.</p>
                 <Link href="https://docs.netlify.com/frameworks/next-js/overview/" className="btn btn-lg sm:min-w-64">
                     Read the Docs
