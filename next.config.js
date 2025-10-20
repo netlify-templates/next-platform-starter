fc41d63e8554d1711aefe05dc40067ec01edab4c
Philippe Serhal <philippe.serhal@gmail.com>
2025-10-20 18:01:45 -0400

    build: fix build warnings due to implicit ESM

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactCompiler: true,
-  };
-  
-  export default nextConfig;
+  reactCompiler: true
+};
+
+export default nextConfig;
